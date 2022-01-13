1c5b4a551509f6e0476c8d9aee6104df7ed4caa6
Niklas Guertler <niklas.guertler@fst.com>
2022-01-13 13:18:37 +0100

    Support C++11 and C++14

--- overlay_fsm.hh
+++ overlay_fsm.hh
@@ -5,18 +5,23 @@
 #include <utility>
 #include <type_traits>
 
+#define always_inline __attribute__((always_inline))
+
 namespace Helper {
 	/// Hilfsklasse für Tag-Parameter.
 	template <typename T>
 	struct UnionTag {};
 	
+#ifdef __GNUC__
 	/**
 	 * Da GCC & Clang fälschlicherweise erfordern, dass alle nicht-statischen Member einer union literal types sein müssen damit die union selbst ein literal type ist,
 	 * werden die Member in ein "Uninitialized" verpackt. Für literal types ist dies ein trivialer Wrapper, für nicht-literal types enthält Uninitialized statt des Types
 	 * flachen Speicher, der per placement-new als Instanz der Klasse genutzt wird. Dadurch ist Uninitialized immer ein literal type.
+	 *
+	 * Auf literal types kann nicht (mehr) sicher mit std::is_literal geprüft werden, verwende daher is_trivially_destructible, denn GCC & Clang stolpern ohnehin über den Destruktor.
 	 */
 
-	template<typename Type, bool = std::is_literal_type<Type>::value>
+	template<typename Type, bool = std::is_trivially_destructible<Type>::value>
 	struct Uninitialized;
 	
 	/// Spezialisierung für literal types
@@ -34,13 +39,27 @@ namespace Helper {
 	struct Uninitialized<Type, false> {
 		/// Dieser Konstruktor erstellt die Instanz per Placement New und kann daher normalerweise nicht als constexpr genutzt werden. Uninitialized gilt dennoch als literal type.
 		template<typename... Args>
-		constexpr Uninitialized (Args&&... args) {
+#if __cplusplus >= 201402L
+		constexpr
+#endif
+		Uninitialized (Args&&... args) {
 			::new (&storage) Type (std::forward<Args>(args)...);
 		}
-		
+
 		// Speicher für den gewünschten Typ anfordern
 		typename std::aligned_storage <sizeof(Type), std::alignment_of<Type>::value>::type storage;
 	};
+#else
+	/// Für konforme Compiler keine Sonderbehandlung nötig
+	template<typename Type>
+	struct Uninitialized {
+		/// Dieser Konstruktor gibt einfach nur die Argumente weiter. Da "Type" literal ist, ist Uninitalized auch literal und bietet einen constexpr-Konstruktor.
+		template<typename... Args>
+		constexpr Uninitialized (Args&&... args) : storage(std::forward<Args>(args)...) { }
+
+		Type storage;
+	};
+#endif
 	
 	/// Dies ist eine variadische rekursive Union, die für jeden übergebenen Typ eine Member-Variable hat. Solange einer der Typen ein literal type ist, ist auch die union einer und kann per constexpr erstellt werden.
 	template <typename... Types>
@@ -65,7 +84,7 @@ namespace Helper {
 		
 		/// Erstellt eine Instanz des angegebenen Types
 		template <typename... Args>
-		constexpr First* construct (UnionTag<First>, Args&&... args) {
+		First* construct (UnionTag<First>, Args&&... args) {
 			return new (&first.storage) First (std::forward<Args> (args)...);
 		}
 		
@@ -77,7 +96,8 @@ namespace Helper {
 		
 		/// Liefert eine Referenz auf den angegeben Typ (falls dieser der aktive ist)
 		constexpr First& get (UnionTag<First>) {
-			return reinterpret_cast<First&> (first.storage);
+			// Hack: In C++11 ist der Typ "const" wegen constexpr
+			return reinterpret_cast<First&> (const_cast<typename std::remove_const<decltype (first.storage)>::type&> (first.storage));
 		}
 		
 		/// Liefert eine Referenz auf den angegeben Typ (falls dieser der aktive ist)
@@ -88,9 +108,47 @@ namespace Helper {
 		
 		/// Instanz des Typs
 		Uninitialized<First> first;
+
 		/// Rekursionsschritt auf nächste Teil-Union
 		MultiUnion<Rest...> rest;
 	};
+
+#if __cplusplus >= 201703L
+	template <bool... V>
+	struct And {
+		static constexpr bool value = (V && ...);
+	};
+	template <bool... V>
+	struct Or {
+		static constexpr bool value = (V || ...);
+	};
+#else
+	template <bool... V>
+	struct And {
+		static constexpr bool value = std::is_same<And, And<(static_cast<void> (V), true)...>>::value;
+	};
+	template <bool... V>
+	struct Or {
+		static constexpr bool value = !And<!V...>::value;
+	};
+#endif
+
+	/**
+	 * Mixin für OverlayFSM; ruft this->destroy() NUR auf, wenn TriviallyDestructible=false ist.
+	 * Damit wird der Destruktor des aktuellen Zustands aufgerufen, wenn die OverlayFSM-Instanz gelöscht wird und der Destruktor nicht-trivial ist;
+	 * ist der Destruktor trivial/nicht vorhanden, hat MixinDestructor und damit auch OverlayFSM keinen Destruktur und ist damit auch trivially destructible.
+	 */
+	template <typename Top, bool TriviallyDestructible>
+	class MixinDestructor;
+
+	template <typename Top>
+	class MixinDestructor<Top, false> {
+		public:
+			~MixinDestructor () { static_cast<Top*> (this)->destroy (); }
+	};
+
+	template <typename Top>
+	class MixinDestructor<Top, true> {};
 }
 
 /**
@@ -104,18 +162,29 @@ namespace Helper {
  * @param States		Die Zustandsklassen, müssen von StateBase_ ableiten
  */
 template <typename StateBase_, typename... States>
-class OverlayFSM {
+class OverlayFSM : public Helper::MixinDestructor<OverlayFSM<StateBase_, States...>, std::is_trivially_destructible<StateBase_>::value> {
+	friend class Helper::MixinDestructor<OverlayFSM<StateBase_, States...>, std::is_trivially_destructible<StateBase_>::value>;
 	public:
 		using StateBase = StateBase_;
 		
 		// Konsistenzprüfungen der Parameter
 		static_assert (sizeof...(States) > 0, "OverlayFSM must have at least one state!");
-		static_assert ((std::is_base_of<StateBase, States>::value && ...), "All states must be derived from the StateBase class!");
-		static_assert ((!std::is_abstract<States>::value && ...), "State classes may not contain pure virtual member functions!");
+		static_assert (Helper::And<std::is_base_of<StateBase, States>::value...>::value, "All states must be derived from the StateBase class!");
+		static_assert (Helper::And<!std::is_abstract<States>::value...>::value, "State classes may not contain pure virtual member functions!");
 	private:
 		Helper::MultiUnion<States...> m_memory;
 		// Zeiger auf den aktuellen Zustand; zeigt in m_stateMem hinein.
 		StateBase* m_current;
+
+		/// Ruft den Destruktor des aktuellen Zustands, auf dem Typ der Basisklasse, auf, falls er nicht-trivial ist. Tut nichts falls der Destruktor trivial ist (SFINAE).
+		template <typename X = StateBase, class = typename std::enable_if<!std::is_trivially_destructible<X>::value>::type>
+		void destroy () {
+			current ().~StateBase ();
+		}
+
+		template <typename X = StateBase, class = typename std::enable_if<std::is_trivially_destructible<X>::value>::type>
+		void destroy (...) {
+		}
 	public:
 		/**
 		 * Initialisiert die FSM mit dem gewünschten Zustand. Der erste Parameter kann einen beliebigen Wert (zB nullptr) haben,
@@ -125,15 +194,12 @@ class OverlayFSM {
 		 * @param args	Parameter, die an den Konstruktor des Initialzustands übergeben werden.
 		 */
 		template <typename S, typename... Args>
-		my_always_inline constexpr OverlayFSM (S*, Args&&... args) : m_memory {Helper::UnionTag<S> {}, std::forward<Args> (args)...}, m_current { &m_memory.get (Helper::UnionTag<S> {}) } {  }
-
-		/// Ruft den Destruktor des aktuellen Zustands, auf dem Typ der Basisklasse, auf.
-		my_always_inline ~OverlayFSM () { current ().~StateBase (); }
+		always_inline constexpr OverlayFSM (S*, Args&&... args) : m_memory {Helper::UnionTag<S> {}, std::forward<Args> (args)...}, m_current { &m_memory.get (Helper::UnionTag<S> {}) } {  }
 
 		/// Gibt eine Referenz auf den aktuellen Zustand zurück, als Typ der Basisklasse.
-		my_always_inline StateBase& current () { return *m_current; }
+		always_inline StateBase& current () { return *m_current; }
 		/// Gibt eine konstante Referenz auf den aktuellen Zustand zurück, als Typ der Basisklasse.
-		my_always_inline const StateBase& current () const { return *m_current; }
+		always_inline const StateBase& current () const { return *m_current; }
 
 		/**
 		 * Wechselt in einen neuen Zustand über. Ruft den Destruktor des aktuellen Zustands, auf dem Typ der Basisklasse, auf.
@@ -143,14 +209,17 @@ class OverlayFSM {
 		 * @return		Die Instanz des neuen Zustands
 		 */
 		template <typename S, typename... Args>
-		my_always_inline S& go (Args&&... args) {
+		always_inline S& go (Args&&... args) {
 			// Prüfe, ob der Zustand auch in den Klassem-Template-Parametern (States...) enthalten ist
-			static_assert ((std::is_same<States, S>::value || ...), "Nonexisting state passed to OverlayFSM::go");
+			static_assert (Helper::Or<std::is_same<States, S>::value...>::value, "Nonexisting state passed to OverlayFSM::go");
 			
 			// Destruktor aufrufen
-			current ().~StateBase ();
+			destroy ();
 			
-			return *m_memory.construct (Helper::UnionTag<S> {}, std::forward<Args> (args)...);
+			S& s = *m_memory.construct (Helper::UnionTag<S> {}, std::forward<Args> (args)...);
+			m_current = &s;
+
+			return s;
 		}
 };
 
